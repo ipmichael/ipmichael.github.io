d59aa39a6f84fa51cc710679ab154e732c780ae4
Michael Ip <ip.w.michael@gmail.com>
2015-09-29 15:59:08 -0400

    le sigh, old background picture took too long to load

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
-// hello world
+function displayVisits() 
+ {    
+  // How many visits so far? 
+      var numVisits = GetCookie("numVisits"); 
+      if (numVisits) numVisits = parseInt(numVisits) + 1; 
+      else numVisits = 1; // the value for the new cookie 
+  
+  // Show the number of visits 
+      if (numVisits==1){
+      	document.write("<p>This is your first visit.</p>");
+      }  
+      else{
+      	document.write("<p>You have visited this page " + numVisits + " times.</p>"); 
+      } 
+  
+  // Set the cookie to expire 365 days from now 
+      var today = new Date(); 
+      today.setTime(today.getTime() + 365 /*days*/ * 24 /*hours*/ * 60 /*minutes*/ * 60 /*seconds*/ * 1000 /*milliseconds*/); 
+      SetCookie("numVisits", numVisits, today); 
+  }
+
+displayVisits();
